b98fb944d41b35a6d50eb131246f829bba0a5561
molul <luismorcillo@gmail.com>
2026-02-18 11:15:19 +0100

    feat: new BeatIndictor

---
+++ src/components/BeatIndicator.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useMetronomeStore } from '../stores/useMetronomeStore'
+
+const store = useMetronomeStore()
+
+const currentBeatInBar = computed(() => {
+  return store.beatInBar === 0 ? 3 : store.beatInBar - 1
+})
+</script>
+
+<template>
+  <div class="flex gap-1 h-2 w-full px-4">
+    <div
+      v-for="i in 4"
+      :key="`beat-${i}`"
+      :class="[
+        'w-full border ',
+        {
+          'bg-green-400 border-green-500': currentBeatInBar === i - 1,
+          'bg-zinc-700 border-zinc-600': currentBeatInBar !== i - 1
+        }
+      ]"
+    >
+      &nbsp;
+    </div>
+  </div>
+</template>
