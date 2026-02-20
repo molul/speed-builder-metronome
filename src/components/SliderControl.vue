3ea7c04d4e6a6483e1ed3b7178c4798aeb451e85
molul <luismorcillo@gmail.com>
2026-02-20 19:10:53 +0100

    feat: missing file from previous commit

--- src/components/SliderControl.vue
+++ src/components/SliderControl.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
 import Label from './Label.vue'
+import Slider from 'primevue/slider'
+
 interface Props {
   label: string
   min: number
   max: number
   step?: number
+  externalMin?: number
+  externalMax?: number
 }
 
 const props = withDefaults(defineProps<Props>(), {
   step: 1
 })
-const model = defineModel<number>()
+
+const model = defineModel<number>({ required: true })
+
+function handleSlideEnd() {
+  if (props.externalMin && model.value <= props.externalMin) {
+    model.value = props.externalMin
+  }
+
+  if (props.externalMax && model.value >= props.externalMax) {
+    model.value = props.externalMax
+  }
+}
 </script>
 
 <template>
@@ -18,13 +33,24 @@ const model = defineModel<number>()
     <Label :label="props.label" class="w-30 text-left" />
     <div class="flex items-center gap-4 flex-1">
       <Slider
+        ref="sliderRef"
         v-model="model"
         :step="props.step"
         :min="props.min"
         :max="props.max"
         class="w-full"
+        @slideend="handleSlideEnd"
       />
-      <span class="w-10 text-right">{{ model }}</span>
+      <span
+        class="w-10 text-right font-mono"
+        :class="{
+          'text-orange-500 font-bold':
+            (props.externalMin && model < props.externalMin) ||
+            (props.externalMax && model > props.externalMax)
+        }"
+      >
+        {{ model }}
+      </span>
     </div>
   </div>
 </template>
