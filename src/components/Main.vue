99e860c27e256d593f5d8a296427ab9472254c0d
molul <luismorcillo@gmail.com>
2026-02-20 14:04:31 +0100

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -67,9 +67,9 @@ async function handleInstall() {
   <div
     class="size-full text-sm text-zinc-900 dark:text-zinc-100 transition-colors duration-500"
   >
-    <div class="size-full sm:max-w-sm sm:m-auto flex overflow-y-auto">
+    <div class="size-full sm:max-w-sm sm:m-auto flex shadow-md overflow-y-auto">
       <div
-        class="size-full sm:rounded-lg flex flex-col gap-0 relative p-0 sm:border border-zinc-300 dark:border-zinc-700 shadow-md overflow-hidden bg-white dark:bg-zinc-900"
+        class="size-full sm:rounded-lg flex flex-col gap-0 relative p-0 sm:border border-zinc-300 dark:border-zinc-700 overflow-hidden bg-white dark:bg-zinc-900"
       >
         <div
           v-if="installPrompt"
